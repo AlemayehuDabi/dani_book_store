cc5b2f1ebd4ec169943218513f44367eca1ff1c4
Alemayehu <alemayehudabi606@gmail.com>
2025-11-17 09:19:01 +0300

    feat: sign up action funciton

---
+++ src/routes/auth/signup/+page.server.ts
@@ -0,0 +1,45 @@
+import { lucia } from '$lib/server/lucia';
+import prisma from '$lib/server/prisma';
+import { fail, redirect, type Actions } from '@sveltejs/kit';
+import { Argon2id } from 'oslo/password';
+
+export const actions: Actions = {
+  default: async ({ request, cookies }) => {
+    const data = await request.formData();
+
+    const { name, email, password } = Object.fromEntries(data) as Record<
+      string,
+      string
+    >;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      return fail(400, 'User already exist');
+    }
+
+    const hashedPassword = await new Argon2id().hash(password);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const session = await lucia.createSession(newUser.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+
+    cookies.set(sessionCookie.name, sessionCookie.value, {
+      path: '.',
+      ...sessionCookie.attributes,
+    });
+
+    redirect(201, '/');
+  },
+};
